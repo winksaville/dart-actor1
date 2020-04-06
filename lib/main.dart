1753bfb9d54d20200a9d49f29b303f20aa13ebac
Wink Saville <wink@saville.com>
2020-04-05 18:24:17 -0700

    Change to the notion of Actors
    And creates two actor1 and actor2 and the "master" (i.e. the main isolate)
    can communicate with both either locally or in a separate isolate.
    
    The next goal is to allow the two actors to communicate with each other
    not just with the master.
    
     - Remove class Client and Servier which only worked "locally"
     - Added class ActorBase and ActorCmd
    
    The output for run is:
    
     $ make run
     dart2native lib/main.dart -o bin/main
     Generated: /home/wink/prgs/dart/isolate-example2/bin/main
     bin/main
     Press any key to stop:
     actor1.entryPoint:+
     actor1.entryPoint:-
     actor1MasterReceivePort: got ActorCmd.sendPort
     actor1._process: data data=Yo Dude
     actor2.entryPoint:+
     actor2.entryPoint:-
     actor2MasterReceivePort: got ActorCmd.sendPort
     actor2._process: data data=Yo Bro

--- lib/main.dart
+++ lib/main.dart
@@ -1,188 +1,114 @@
 // Based on [this](https://github.com/winksaville/dart-isolate-example)
 
+// Currentlly creates two actors and they can communicate with the "master".
+// The next goal is to implement the "connect" command so the two actors
+// can communicate directly with them selves.
+
 import 'dart:io';
-import 'dart:async';
+//import 'dart:async';
 import 'dart:isolate';
-import 'package:intl/intl.dart';
+//import 'package:intl/intl.dart';
 
-class ResultSCS {
-  MyClient client;
-  MyServer server;
-  ResultSCS(this.client, this.server);
+/// Commands actors support
+/// A message is a Map with a key of ActorCmd whose
+/// name is one of the following enumerations
+enum ActorCmd {
+  sendPort,  /// ActorCmd.data is the SendPort
+  data,      /// ActorCmd.data is some data
+  connect,   /// ActorCmd.data is TBD
 }
 
-// Start a server and client
-Future<ResultSCS> startServerAndClient({bool locally=false}) async {
-  // Create a port used to communite with the isolate
-  ReceivePort receivePort = ReceivePort();
-  SendPort responsePort = null;
+/// Actor that can process messages
+abstract class ActorBase {
+  String name;
+  Isolate isolate;
 
-  // Create Server
-  Server server = MyServer(receivePort);
+  SendPort _toMasterSendPort;
+  ReceivePort _fromMasterReceivePort;
 
-  // Create client
-  Client client = MyClient(receivePort.sendPort);
-
-  // Start the client
-  await client.start(locally: locally);
-
-  // Start the server
-  await server.start(locally: locally);
-
-  // Return the client and server
-  return ResultSCS(client, server);
-}
-
-/// Client that can process messages and communicates
-/// via messages using a ReceivePort and SendPort.
-abstract class Client {
-  SendPort _partnerPort;
-  ReceivePort _receivePort;
-  Isolate _isolate;
-
-  /// Contruct a client passing a SendPort which is used
+  /// Contruct an actor passing a SendPort which is used
   /// by the client to send messages to the ReceivePort.
-  Client(SendPort partnerPort) {
-    _partnerPort = partnerPort;
-  }
+  ActorBase(String this.name, SendPort this._toMasterSendPort);
 
-  /// Start the client. When the client starts _enter will
-  /// be invoked and by default it creates _receivePort
-  /// and executes _partnerPort.send(_receivePort.sendPort)
-  /// which sends the SendPort to the partner.
+  /// Start the actor. When the actor starts _enter will
+  /// it will create a ReceivePort and send the SendPort
+  /// back to the master.
   void start({bool locally=false}) async {
     if (locally) {
-      _isolate = Isolate.current;
+      isolate = Isolate.current;
       _entryPoint(this);
     } else {
-      _isolate = await Isolate.spawn<Client>(Client._entryPoint, this);
+      isolate = await Isolate.spawn<ActorBase>(ActorBase._entryPoint, this);
     }
   }
 
-  /// Stop the client immediately if its not a local client.
+  /// Stop the actor immediately if its not a local actor.
   void stop() {
     // Handle isolate being null
-    if ((_isolate != null) && (_isolate != Isolate.current)) {
-      _isolate.kill(priority: Isolate.immediate);
+    if ((isolate != null) && (isolate != Isolate.current)) {
+      isolate.kill(priority: Isolate.immediate);
     }
   }
 
-  /// Invoked by when the clients starts.
-  static void _entryPoint(Client client) {
-    client._enter();
-    client._begin();
-    client._receivePort.listen(client._process);
-    stdout.writeln('client: running');
+  /// Invoked by when the actor starts.
+  static void _entryPoint(ActorBase actor) {
+    stdout.writeln('${actor.name}.entryPoint:+');
+    actor._enter();
+    actor._begin();
+    actor._listen();
+    stdout.writeln('${actor.name}.entryPoint:-');
   }
 
-  /// Called once when Client is first invoked and usually
+  /// Called once when Actor is first invoked and usually
   /// Sets up communication with the partner
   void _enter() {
-    // Create a port that will receive messages from our partner
-    _receivePort = ReceivePort();
-
-    // Using the partnerPort send our sendPort so they
-    // can send us messages.
-    _partnerPort.send(_receivePort.sendPort);
+    // Create a port that will receive messages
+    _fromMasterReceivePort =  ReceivePort();
+    _toMasterSendPort.send({
+      ActorCmd: ActorCmd.sendPort,
+      ActorCmd.data: _fromMasterReceivePort.sendPort
+    });
   }
 
-  /// Invoked once when the client is first started after
+  /// Invoked once when the actor is first started after
   /// _enter and before the first call to _process.
   void _begin() {}
 
+  void _listen() {
+    assert(_fromMasterReceivePort != null);
+    _fromMasterReceivePort.listen(_process);
+  }
+
   /// Invoked everytime a message arrives from the partner
   /// and must be overridden
   void _process(message);
 }
 
-class MyClient extends Client {
+class MyActor extends ActorBase {
   int counter = 0;
 
-  MyClient(SendPort partnerPort) : super(partnerPort);
+  MyActor(String name, SendPort masterPort) : super(name, masterPort);
 
   @override
   void _begin() {
-    counter = 1;
-    _partnerPort.send(counter);
   }
 
   @override
-  void _process(dynamic message) {
-      //stdout.writeln('RESP: ' + data);
-      counter++;
-      //stdout.writeln('SEND: ' + counter);
-      _partnerPort.send(counter);
+  void _process(dynamic msg) {
+    switch (msg[ActorCmd]) {
+      case ActorCmd.sendPort:
+        stdout.writeln('${name}._process: sendPort data=${msg[ActorCmd.data]}');
+        break;
+      case ActorCmd.connect:
+        stdout.writeln('${name}._process: connect data=${msg[ActorCmd.data]}');
+        break;
+      case ActorCmd.data:
+        stdout.writeln('${name}._process: data data=${msg[ActorCmd.data]}');
+        break;
+    }
   }
 }
 
-/// Server
-abstract class Server {
-  SendPort _partnerPort;
-  ReceivePort _receivePort;
-  Isolate _isolate;
-
-  /// Contruct a server passing a ReceivePort which is used
-  /// by the server to receive messages from clients.
-  Server(ReceivePort receivePort) {
-    _receivePort = receivePort;
-  }
-
-  /// Start the server.
-  void start({bool locally=false}) async {
-    if (locally) {
-      _isolate = Isolate.current;
-      _entryPoint(this);
-    } else {
-      _isolate = await Isolate.spawn<Server>(Server._entryPoint, this);
-    }
-  }
-
-  /// Stop the server immediately if its not a local client.
-  void stop() {
-    // Handle isolate being null
-    if ((_isolate != null) && (_isolate != Isolate.current)) {
-      _isolate.kill(priority: Isolate.immediate);
-    }
-  }
-
-  /// Invoked by when the server starts.
-  static void _entryPoint(Server server) {
-    server._begin();
-    server._receivePort.listen(server.__process);
-    stdout.writeln('server: running');
-  }
-
-  /// Invoked once when the server and before the first call to _process.
-  void _begin() {}
-
-  /// Closed by for every message and handles receiving the _partnerPort
-  /// which is expected to be the first message.
-  void __process(dynamic message) {
-    if (message is SendPort) {
-      stdout.writeln('MyServer: got partnerPort');
-      _partnerPort = message;
-    } else {
-      assert(_partnerPort != null);
-      _process(message);
-    }
-  }
-
-  /// Invoked everytime a message arrives from the partner
-  /// and must be overridden
-  void _process(message);
-}
-
-class MyServer extends Server {
-  int counter = 0;
-
-  MyServer(ReceivePort receivePort) : super(receivePort);
-
-  @override
-  void _process(dynamic message) {
-    counter += 1;
-    _partnerPort.send(message);
-  }
-}
 
 void main() async {
   // Change stdin so it doesn't echo input and doesn't wait for enter key
@@ -195,32 +121,83 @@ void main() async {
   // Tell the user to press a key
   stdout.writeln('Press any key to stop:');
 
-  int beforeStart = stopwatch.elapsedMicroseconds;
+  //int beforeStart = stopwatch.elapsedMicroseconds;
 
-  // Start client and server
-  ResultSCS result = await startServerAndClient(locally: true);
+  // Create actor1
+  ReceivePort actor1MasterReceivePort = ReceivePort();
+  MyActor actor1 = MyActor('actor1', actor1MasterReceivePort.sendPort);
+  await actor1.start(locally: false);
+  SendPort actor1ToActorSendPort = null;
+  actor1MasterReceivePort.listen((msg) {
+    switch (msg[ActorCmd]) {
+      case ActorCmd.sendPort:
+        stdout.writeln('actor1MasterReceivePort: got ActorCmd.sendPort');
+        assert(msg[ActorCmd.data] is SendPort);
+        actor1ToActorSendPort = msg[ActorCmd.data];
+        actor1ToActorSendPort.send({
+          ActorCmd: ActorCmd.data,
+          ActorCmd.data: "Yo Dude",
+        });
+        break;
+      case ActorCmd.connect:
+        stdout.writeln('actor1MasterReceivePort: connect msg=${msg}');
+        break;
+      case ActorCmd.data:
+        stdout.writeln('actor1MasterReceivePort: data msg=${msg}');
+        break;
+      default:
+        stdout.writeln('actor1MasterReceivePort: unknown msg=${msg}');
+    }
+  });
 
-  // Wait for any key
-  int afterStart = stopwatch.elapsedMicroseconds;
+  // Create actor2
+  ReceivePort actor2MasterReceivePort = ReceivePort();
+  MyActor actor2 = MyActor('actor2', actor2MasterReceivePort.sendPort);
+  await actor2.start(locally: false);
+  SendPort actor2ToActorSendPort = null;
+  actor2MasterReceivePort.listen((msg) {
+    switch (msg[ActorCmd]) {
+      case ActorCmd.sendPort:
+        stdout.writeln('actor2MasterReceivePort: got ActorCmd.sendPort');
+        assert(msg[ActorCmd.data] is SendPort);
+        actor2ToActorSendPort = msg[ActorCmd.data];
+        actor2ToActorSendPort.send({
+          ActorCmd: ActorCmd.data,
+          ActorCmd.data: "Yo Bro",
+        });
+        break;
+      case ActorCmd.connect:
+        stdout.writeln('actor2MasterReceivePort: connect msg=${msg}');
+        break;
+      case ActorCmd.data:
+        stdout.writeln('actor2MasterReceivePort: data msg=${msg}');
+        break;
+      default:
+        stdout.writeln('actor2MasterReceivePort: unknown msg=${msg}');
+    }
+  });
+
+  //// Wait for any key
+  //int afterStart = stopwatch.elapsedMicroseconds;
   await stdin.first;
-  int done = stopwatch.elapsedMicroseconds;
+  //int done = stopwatch.elapsedMicroseconds;
 
-  // Stop the client
-  stdout.writeln('stopping');
-  result.client.stop();
-  result.server.stop();
-  stdout.writeln('stopped');
+  //// Stop the client
+  //stdout.writeln('stopping');
+  //result.client.stop();
+  //result.server.stop();
+  //stdout.writeln('stopped');
 
-  // Print time
-  int msgCounter = result.client.counter + result.server.counter;
-  double totalSecs = (done.toDouble() - beforeStart.toDouble()) / 1000000.0;
-  double rate = msgCounter.toDouble() / totalSecs;
-  NumberFormat f3digits = NumberFormat('###,###.00#');
-  NumberFormat f0digit = NumberFormat('###,###');
-  stdout.writeln(
-    'Total time=${f3digits.format(totalSecs)} secs '
-    'msgs=${f0digit.format(msgCounter)} '
-    'rate=${f0digit.format(rate)} msgs/sec');
+  //// Print time
+  //int msgCounter = result.client.counter + result.server.counter;
+  //double totalSecs = (done.toDouble() - beforeStart.toDouble()) / 1000000.0;
+  //double rate = msgCounter.toDouble() / totalSecs;
+  //NumberFormat f3digits = NumberFormat('###,###.00#');
+  //NumberFormat f0digit = NumberFormat('###,###');
+  //stdout.writeln(
+  //  'Total time=${f3digits.format(totalSecs)} secs '
+  //  'msgs=${f0digit.format(msgCounter)} '
+  //  'rate=${f0digit.format(rate)} msgs/sec');
 
   // Because main is async use exit
   exit(0);
